22c050425a2c08335ba0c00512eddce2a36e9fdd
Yun Dayeong <ydiosa98@gmail.com>
2025-07-08 12:00:53 +0900

    first commit

---
+++ src/main/java/com/example/reframe/BnkProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.reframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnkProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnkProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reframe/BnkProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnkProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
