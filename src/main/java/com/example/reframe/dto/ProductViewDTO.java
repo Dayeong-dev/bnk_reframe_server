ca085fcb3cabce460e89ccbeca6226ebd6f51944
Jin-monk <beobjin-_-@naver.com>
2025-07-11 14:49:07 +0900

    [beobjin] 관리자 상품 조회수 완료 2 // 20250711 14:49 //

---
+++ src/main/java/com/example/reframe/dto/ProductViewDTO.java
@@ -0,0 +1,12 @@
+package com.example.reframe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductViewDTO {
+
+	 private String name;
+	 private Long viewCount;
+}
